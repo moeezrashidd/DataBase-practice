088f8967fd7862e6e1f7779aaebf0ff1b6d041e5
Moeez Rashid <moeezrashidd@gmail.com>
2026-04-08 06:43:29 +0500

    tables are created

--- set Functions.sql
+++ set Functions.sql
@@ -1,1 +1,28 @@
-this is for set functions to practice
+CREATE DATABASE SQL_SetFunction_Practice;
+USE SQL_SetFunction_Practice;
+
+CREATE TABLE Employees (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    DepartmentID INT,
+    Salary INT,
+    HireDate DATE,
+    City VARCHAR(50)
+);
+
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
+);
+CREATE TABLE Projects (
+    ProjectID INT PRIMARY KEY,
+    ProjectName VARCHAR(50),
+    Budget INT
+);
+
+CREATE TABLE EmployeeProjects (
+    EmpID INT,
+    ProjectID INT,
+    HoursWorked INT,
+    PRIMARY KEY (EmpID, ProjectID)
+);
